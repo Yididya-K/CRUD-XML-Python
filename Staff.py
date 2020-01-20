9cca604e67fd2992faf09bf1ca09bb3fc5e0f796
Yididya-K <kebedeyididya1@gmail.com>
2020-01-20 05:25:23 +0300

    crud xml in python

---
+++ Staff.py
@@ -0,0 +1,210 @@
+import xml.etree.cElementTree as ET
+
+
+
+def stafftMenu():
+    print("")
+    print("STAFF DATABASE")
+    print("1 -Show all students")
+    print("2 -Search for student")
+    print("3 -Edit a student")
+    print("4 -Create a student")
+    print("5 -Delete a student")
+    print("x -Exit")
+    userinput=input("Choose option 1-5 or 'x' to exit: ")
+    return userinput
+
+def showAllStudents():
+    for contact in root.findall('contact'):
+        name = contact.find('name').text
+        address = contact.find('address').text
+        phone = contact.find('phone').text
+        age = contact.find('age').text
+        email= contact.find('email').text
+        id= contact.find('id').text
+        print("--------------------------------------------")
+        print("ID: "+ id)        
+        print("Name: "+ name)
+        print("Address: "+ address)
+        print("Phone: "+ phone)
+        print("Email: "+ email)
+        print("Age: "+ age)
+    print("--------------------------------------------")
+    print("End of list")
+
+
+def searchForStaff():
+    
+    email =input("Enter the Email address of the staff member you want to search for: ")
+    
+    
+    for contact in root.findall('contact'):
+        emailb = contact.find('email').text
+        print("email1: "+ email +" email2: "+ emailb)
+
+        if emailb==email:
+            
+            id = contact.find('id').text
+            name = contact.find('name').text
+            address = contact.find('address').text
+            phone = contact.find('phone').text
+            age = contact.find('age').text
+            
+            print("--------------------------------------------")
+            print("STAFF RECORD ID: "+ id)
+            print("ID: "+ id)        
+            print("Name: "+ name)
+            print("Address: "+ address)
+            print("Phone: "+ phone)
+            print("Email: "+ emailb)
+            print("Age: "+ age)
+            print("--------------------------------------------")
+
+
+def recordExists(email):
+
+    
+    retValue=False
+
+    
+    for contact in root.findall('contact'):
+        emailb = contact.find('email').text
+        
+        
+        if emailb==email:
+        
+            retValue=True
+   
+    return retValue
+
+
+
+def editStudent():
+   
+    email =input("Enter the Email address of the staff member to edit: ")
+
+    
+    for contact in root.findall('contact'):
+        emailb = contact.find('email').text
+
+      
+        if emailb==email:
+            
+            id = contact.find('id').text
+            name = contact.find('name').text
+            address = contact.find('address').text
+            phone = contact.find('phone').text
+            email = contact.find('email').text
+            age = contact.find('age').text
+
+        
+            name = input("Please enter name:(" + name + ")") or name
+            contact.find('name').text= name
+           
+            address = input("Please enter address:(" + address + ")") or address
+            contact.find('address').text= address
+           
+            phone = input("Please enter phone:(" + phone + ")") or phone
+            contact.find('phone').text= phone
+            
+            phone = input("Please enter email:(" + email + ")") or email
+            contact.find('email').text= email
+           
+            age = input("Please enter age:(" + age + ")") or age
+            contact.find('age').text= age
+            
+            
+            ntree.write("Staff.xml")            
+            
+            print("--------------------------------------------")
+            print("STAFF RECORD ID: "+ id + " UPDATED")            
+            print("--------------------------------------------")
+
+            
+
+
+def newid():
+    maxid = 0
+    for contact in root.findall('contact'):
+        id= int(contact.find('id').text)
+        if id>maxid:
+            maxid=id
+    return maxid+1
+
+
+def createStaff():
+    
+    email =input("Enter the Email address of the staff member you want to search for: ")    
+    exists=recordExists(email)
+    if exists==False:
+        
+  
+        nid=newid()
+
+       
+        print("Create a record")
+        name=input("Name:")
+        address=input("Address:")
+        age=input("Age:")
+        email=input("Email:")
+        phone=input("Phone:")
+
+      
+        newrecord = ET.SubElement(root, "contact",id=str(nid))
+
+        
+        ET.SubElement(newrecord, "id", name="id").text = str(nid)
+        ET.SubElement(newrecord, "name", name="name").text = name
+        ET.SubElement(newrecord, "address", name="address").text = address
+        ET.SubElement(newrecord, "phone", name="phone").text = phone
+        ET.SubElement(newrecord, "email", name="email").text = email
+        ET.SubElement(newrecord, "age", name="age").text = age
+
+        
+        ntree.write("Staff.xml")    
+
+    else:
+        print("--------------------------------------------")
+        print("Record already exists")
+        print("--------------------------------------------")
+
+
+def deleteStaff():
+
+ 
+    deleterecord=input("Enter the Email address of the record you want to delete: ")
+
+    for contact in root.findall('contact'):
+         
+        emailb= contact.find('email').text
+        
+        if emailb == deleterecord:
+            
+            root.remove(contact)            
+        
+            ntree.write("students.xml")        
+
+
+
+
+ntree = ET.parse('students.xml')
+root = ntree.getroot()
+
+userinput=""
+while userinput !="x":    
+    userinput=studentMenu()
+
+    if userinput =="1":
+        showAllStudents()
+
+    if userinput =="2":
+        searchForStudent()
+
+    if userinput =="3":
+        editStudent()
+
+    if userinput =="4":
+        createStudent()
+
+    if userinput =="5":
+        deleteStudent()
